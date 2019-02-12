c03ef57ccd4f4339878e17cc1024b458f8759bbe
Mihaita A. Lupoiu <mlupoiu@nayarsystems.com>
2019-02-12 10:09:26 +0100

    Blinking Working. Added Kconfig to configure from menuconfig the GPIO to use.

--- main/main.c
+++ main/main.c
@@ -1,16 +1,38 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+/* Can run 'make menuconfig' to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+#define BLINK_TIME 1000
+
+void blink_task(void *pvParameter) {
+	/* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+	   muxed to GPIO on reset already, but some default to other
+	   functions and need to be switched to GPIO. Consult the
+	   Technical Reference for a list of pads and their default
+	   functions.)
+	*/
+	gpio_pad_select_gpio(BLINK_GPIO);
+	/* Set the GPIO as a push/pull output */
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+	while (1) {
+		/* Blink off (output low) */
+		gpio_set_level(BLINK_GPIO, 0);
+		vTaskDelay(BLINK_TIME / portTICK_PERIOD_MS);
+		/* Blink on (output high) */
+		gpio_set_level(BLINK_GPIO, 1);
+		vTaskDelay(BLINK_TIME / portTICK_PERIOD_MS);
+	}
+}
 
 void app_main() {
 	printf("Hello world!\n");
@@ -26,11 +48,5 @@ void app_main() {
 	printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
 	       (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-	for (int i = 10; i >= 0; i--) {
-		printf("Restarting in %d seconds...\n", i);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
-	printf("Restarting now.\n");
-	fflush(stdout);
-	esp_restart();
+	xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 }
